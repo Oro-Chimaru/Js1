ef6d56283afed6c92383520378adb3e645b4e549
Oro-Chimaru <bakuradze11irakli@gmail.com>
2024-07-19 23:38:24 +0400

    first js

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("14 + 14 =", 14 + 14 );
+console.log("14 * 14=", 14 * 14);
+console.log("Hello Orochimaru");
+const firstName = "Khvicha";
+const lastName = "Kvaratskhelia";
+
+const fullName = firstName + " " + lastName;
+console.log(fullName); // Output: "John Doe"
