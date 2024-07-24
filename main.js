c6c63940bfa15cfb3a6d4550be7170c414af12e7
Oro-Chimaru <bakuradze11irakli@gmail.com>
2024-07-24 16:24:01 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,57 @@
-console.log("14 + 14 =", 14 + 14 );
-console.log("14 * 14=", 14 * 14);
-console.log("Hello Orochimaru");
-const firstName = "Khvicha";
-const lastName = "Kvaratskhelia";
+// 1 
+let string = "khvicha kvaratskhelia";
+console.log(string);
 
-const fullName = firstName + " " + lastName;
-console.log(fullName); // Output: "John Doe"
+//2
+// "5" is string, 5 is number
+
+//3 
+let nmb = 77;
+console.log(nmb);
+
+//4
+let bln = true;
+console.log(bln);
+
+//5
+
+//6 
+let hello = "Hello";
+console.log(hello.toUpperCase());
+
+// #7
+let world = "World";
+console.log(world.toLowerCase());
+
+//8
+
+//9
+
+//10
+let text = 'Hello';
+console.log(text.toLowerCase());
+
+//11
+let age = Number(prompt('How old are you?'));
+let nextAge = age + 1;
+console.log(`Next year you will be ${nextAge}`);
+
+//12
+let name = prompt('What is your name?');
+let agee = Number(prompt('How old are you?'));
+let isInSixties = agee >= 60;
+console.log(`${name} is in sixties: ${isInSixties}`);
+
+//13 
+// value of the X is 6 
+
+//14
+// value of Y is 5
+
+//18 
+console.log(1 == 1);
+
+console.log('hello' == 'hello');
+
+//19 
+console.log(14 != 19)
